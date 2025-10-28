9b0a4e60df803b12bad67a3aebd7b68719228df4
Dominic Myers <annoyingmouse@gmail.com>
2025-10-28 09:24:04 +0000

    chore(build): Configure Rollup for optimized output

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
-import { terser } from 'rollup-plugin-terser'
+import { terser } from "rollup-plugin-terser";
+import stripTemplateWhitespace from './plugins/stripTemplateWhitespace.js';
 
 export default {
-  input: './wc-pagination.js',
+  input: "./wc-pagination.js",
   output: {
-    file: 'dist/wc-pagination.min.js',
-    format: 'iife',
-    sourcemap: 'inline',
+    file: "dist/wc-pagination.min.js",
+    format: "iife",
+    sourcemap: false,
   },
-  plugins: [
-    terser()
-  ],
-}
+  plugins: [terser(), stripTemplateWhitespace({
+    html: true,      // remove spaces between tags
+    tightText: true, // also trim text-node edges for max compression
+    // include: ['src/**'], // optional
+    // exclude: ['**/*.test.*'], // optional
+  })],
+};
