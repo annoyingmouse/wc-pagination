c9bfb92a758bc325b83801005ab2f0ee77a22643
Dominic Myers <annoyingmouse@gmail.com>
2025-10-30 07:53:11 +0000

    More testing woes

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,23 @@
 export default {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["./jest.setup.js"],
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
-
-  // Only collect coverage from the component file:
-  collectCoverageFrom: ["**/wc-pagination.js"],
-
-  // Optional: fail builds if coverage drops below target
-  coverageThreshold: {
-    "**/wc-pagination.js": {
-      branches: 80,
-      functions: 85,
-      lines: 90,
-      statements: 90,
-    },
-  },
+  /**
+   * This following causes issues with `np`
+   */
+  // collectCoverage: true,
+  // coverageDirectory: "coverage",
+  // coverageReporters: ["text", "lcov", "html"],
+  //
+  // // Only collect coverage from the component file:
+  // collectCoverageFrom: ["**/wc-pagination.js"],
+  //
+  // // Optional: fail builds if coverage drops below target
+  // coverageThreshold: {
+  //   "**/wc-pagination.js": {
+  //     branches: 80,
+  //     functions: 85,
+  //     lines: 90,
+  //     statements: 90,
+  //   },
+  // },
 };
